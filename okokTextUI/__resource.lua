44e1f4a29a4c240106d4f9c9fa429f35c18fd2ae
SPY IV <109961542+Spy-IV@users.noreply.github.com>
2023-04-24 15:10:30 +0530

    Add files via upload

---
+++ okokTextUI/__resource.lua
@@ -0,0 +1,15 @@
+ui_page 'web/ui.html'
+
+client_scripts {
+	'client.lua',
+}
+
+files {
+	'web/scripts.js',
+	'web/sound.mp3',
+	'web/styles.css',
+	'web/ui.html',
+}
+
+export 'Open'
+export 'Close'
